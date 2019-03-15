0b90cd04496d7f96eb9ea29c1a331a9db36f4914
mverost <mverost4422@gmail.com>
2019-03-14 21:34:13 -0400

    Added production api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://lit-reef-77205.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
